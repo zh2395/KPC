e3cf6d74fbce27b429a1ebcdde2fac7e55574931
Zhen Huang <zh2395@columbia.edu>
2020-10-20 22:53:09 -0400

    Faster computation for Gaussian and linear kernel. Exact computation for CME.

--- R/KPC.R
+++ R/KPC.R
@@ -1,15 +1,50 @@
-#' Tn with Knn graph
+#' Tn with geometric graphs
 #'
-#' Calculate Tn using directed Knn graph.
+#' Calculate Tn using directed Knn graph or minimum spanning tree.
 #'
 #' Tn is an estimate of E[E[k(Y1,Y1')|X]], with Y1, Y1' drawn iid from Y|X, given X.
+#' Kernels provided in the package "kernlab" are recommended, e.g. rbfdot(sigma), vanilladot().
+#' For continuous data, "breaktie" can be set to FALSE for faster computation.
 #'
 #' @param X Matrix of predictors (n by dx)
 #' @param Y Matrix of response (n by dy)
 #' @param k a kernel function k(y,y')
-#' @param Knn The number of K-nearest neighbor to use
+#' @param Knn The number of K-nearest neighbor to use; or "MST".
 #' @return Tn: a numeric number
 TnKnn = function(Y,X,k,Knn=1) {
+  if (Knn == "MST") {
+    if(!is.data.frame(X)) {
+      X = as.data.frame(X)
+    }
+    if(!is.matrix(Y)) {
+      Y = as.matrix(Y)
+    }
+    n = dim(Y)[1]
+
+    if (dim(X)[2] == 1) {
+      Y = Y[order(X),]
+      if(!is.matrix(Y)) {
+        Y = as.matrix(Y)
+      }
+      node_calculator = function(j) {
+        return(k(Y[j,],Y[j-1,]) + k(Y[j,],Y[j+1,]))
+      }
+      return((sum(sapply(2:(n-1), node_calculator))/2 + k(Y[1,],Y[2,]) + k(Y[n-1,],Y[n,]))/n)
+    }
+
+    out=emstreeR::ComputeMST(X,verbose = F)
+    tmp = matrix(0,n,2)
+    # the first column is the degree of node i
+    # the second column is the sum of k(xi,x_{N(i)})
+    for (i in 1:(length(out$from)-1)) {
+      tmp[out$from[i],1] = tmp[out$from[i],1] + 1
+      tmp[out$to[i],1] = tmp[out$to[i],1] + 1
+      tmp[out$from[i],2] = tmp[out$from[i],2] + k(Y[out$to[i],],Y[out$from[i],])
+      tmp[out$to[i],2] = tmp[out$to[i],2] + k(Y[out$to[i],],Y[out$from[i],])
+    }
+    return(mean(tmp[,2]/tmp[,1]))
+  }
+
   id <- group <- NULL
   if(!is.matrix(X)) {
     X = as.matrix(X)
@@ -83,12 +118,41 @@ TnKnn = function(Y,X,k,Knn=1) {
     }
   }
 
-  node_calculator = function(j) {
-    sum = 0
-    for (i in 1:Knn) {
-      sum = sum + k(Y[j,],Y[nn_index_X[j,i],])
+  # faster calculation for Gaussian kernel and linear kernel
+  if (class(k) == "rbfkernel") {
+    sig = kernlab::kpar(k)$sigma
+    if (Knn == 1) {
+      node_calculator = function(j) {
+        return(exp(-sig * sum((Y[j,]-Y[nn_index_X[j,],])^2)))
+      }
     }
-    return(sum)
+    else {
+      node_calculator = function(j) {
+        return(sum(exp(-sig * (outer(sum(Y[j,]^2),base::rowSums(as.matrix(Y[nn_index_X[j,],]^2)),"+") - 2*base::tcrossprod(t(Y[j,]),as.matrix(Y[nn_index_X[j,],]))))))
+      }
+    }
+  }
+  else if (class(k) == "vanilladot") {
+    if (Knn == 1){
+      node_calculator = function(j) {
+        return(sum(Y[j,]*Y[nn_index_X[j,],]))
+      }
+    }
+    else {
+      node_calculator = function(j) {
+        return(sum(base::tcrossprod(t(Y[j,]),Y[nn_index_X[j,],])))
+      }
+    }
+  }
+  else {
+    # calculation for general kernel
+    node_calculator = function(j) {
+      sum = 0
+      for (i in 1:Knn) {
+        sum = sum + k(Y[j,],Y[nn_index_X[j,i],])
+      }
+      return(sum)
+    }
   }
 
   return(mean(sapply(1:n, node_calculator))/Knn)
@@ -106,7 +170,7 @@ TnKnn = function(Y,X,k,Knn=1) {
 #' @param Z Matrix (n by dz)
 #' @param X Matrix (n by dx)
 #' @param k a kernel function k(y,y')
-#' @param Knn number of nearest neighbor to use
+#' @param Knn number of nearest neighbor to use; or "MST"
 #' @param trans_inv TRUE or FALSE. Is kernel k translation invariant?
 #' @details The value returned by KPC can be positive or negative. Asymptotically, it is guaranteed
 #'   to be between 0 and 1. A small value indicates low conditional dependence between Y and Z given X, and
@@ -138,6 +202,8 @@ KPCKnn = function(Y,X,Z,k,Knn = 1,trans_inv=FALSE) {
 #'
 #' Double-centering a squared matrix
 #'
+#' Given a square matrix A, the function return HAH, where H = diag(n) - 1/n is the centering matrix.
+#'
 #' @param M Matrix (n by n)
 double_center = function(M){
   return(M - rowMeans(M) - matrix(colMeans(M),nrow(M),ncol(M),byrow = T) + mean(M[]))
@@ -156,28 +222,48 @@ double_center = function(M){
 #' @param kx kernel function for X
 #' @param kxz kernel function for (X,Z) or for Z if X is empty
 #' @param eps A small regularization parameter for inverting the empirical cross-covariance operator
+#' @param appro whether to use incomplete Cholesky decomposition for approximation
 #' @param tol Tolerance used for incomplete Cholesky decomposition
 #'
 #' @examples
 #' data("medical")
-#' KPCCME(med$D,med$C,med$U,rbfdot(1/(2*median(dist(med$D))^2)),rbfdot(1/(2*median(dist(med$C))^2)), rbfdot(1/(2*median(dist(cbind(med$C,med$U)))^2)), 1e-3,1e-12) # 0.0003175344 D is independent of U given C
-#' KPCCME(med$D,med$U,med$C,rbfdot(1/(2*median(dist(med$D))^2)),rbfdot(1/(2*median(dist(med$U))^2)), rbfdot(1/(2*median(dist(cbind(med$C,med$U)))^2)), 1e-3,1e-12) # 0.6834605 D is associated with C controlling U
-KPCCME = function(Y, X = NULL, Z, ky, kx, kxz, eps, tol = 1e-3) {
-  Y = as.matrix(Y)
-  Z = as.matrix(Z)
+#' KPCCME(med$D,med$C,med$U,rbfdot(1/(2*median(dist(med$D))^2)),rbfdot(1/(2*median(dist(med$C))^2)), rbfdot(1/(2*median(dist(cbind(med$C,med$U)))^2)), 1e-3,F) # 0.0003175344 D is independent of U given C
+#' KPCCME(med$D,med$U,med$C,rbfdot(1/(2*median(dist(med$D))^2)),rbfdot(1/(2*median(dist(med$U))^2)), rbfdot(1/(2*median(dist(cbind(med$C,med$U)))^2)), 1e-3,F) # 0.6834605 D is associated with C controlling U
+KPCCME = function(Y, X = NULL, Z, ky, kx, kxz, eps, appro = TRUE, tol = 1e-3) {
+  if(!is.matrix(Y)) {
+    Y = as.matrix(Y)
+  }
+  if(!is.matrix(Z)) {
+    Z = as.matrix(Z)
+  }
+  n = dim(Y)[1]
+  if (!appro) {
+    # exact computation
+    tilde_Ky = double_center(kernlab::kernelMatrix(ky,Y))
+    if (is.null(X)) {
+      M = diag(n) - n*eps*solve(double_center(kernlab::kernelMatrix(kxz,Z))+n*eps*diag(n))
+      numerator = sum(tilde_Ky * base::crossprod(M))
+      denominator = sum(diag(tilde_Ky))
+      return(numerator/denominator)
+    }
+    else {
+      N = solve(double_center(kernlab::kernelMatrix(kx,X)) + n*eps*diag(n))
+      denominator = sum(tilde_Ky * base::crossprod(N))
+      numerator = sum(tilde_Ky * base::crossprod(solve(double_center(kernlab::kernelMatrix(kxz,cbind(X,Z))) + n*eps*diag(n)) - N))
+      return(numerator/denominator)
+    }
+  }
+  # Approximate computation with incomplete Cholesky decomposition
   if (is.null(X)) {
     Lz = inchol(Z, kxz, tol = tol)
     Lz = Lz - rep(colMeans(Lz), rep.int(n, ncol(Lz)))
     Ly = inchol(Y, ky, tol = tol)
-    Ky = kernlab::kernelMatrix(ky,Y)
-    tilde_Ky = Ky - rowMeans(Ky) - matrix(colMeans(Ky),nrow(Ky),ncol(Ky),byrow = T) + mean(Ky)
-    return(sum((t(Ly)%*%Lz%*%solve(dim(Y)[1]*eps*diag(dim(Lz)[2]) + t(Lz)%*%Lz)%*%t(Lz))^2)/sum(diag(tilde_Ky)))
+    return(sum((t(Ly)%*%Lz%*%solve(dim(Y)[1]*eps*diag(dim(Lz)[2]) + t(Lz)%*%Lz)%*%t(Lz))^2)/sum(diag(double_center(kernlab::kernelMatrix(ky,Y)))))
   }
   X = as.matrix(X)
   L1 = inchol(X, kx, tol = tol)
   L2 = inchol(cbind(X,Z), kxz, tol = tol)
   L3 = inchol(Y, ky, tol = tol)
-  n = dim(X)[1]
   L1 = L1 - rep(colMeans(L1), rep.int(n, ncol(L1)))
   L2 = L2 - rep(colMeans(L2), rep.int(n, ncol(L2)))
   L3 = L3 - rep(colMeans(L3), rep.int(n, ncol(L3)))
@@ -193,7 +279,7 @@ KPCCME = function(Y, X = NULL, Z, ky, kx, kxz, eps, tol = 1e-3) {
 #'
 #' Variable selection with KPC using directed Knn graph
 #'
-#' A stepwise forward selection of variables using KPC. At each step the xj maximizing rho^2(Y,Xj|selected Xi) is selected.
+#' A stepwise forward selection of variables using KPC. At each step the Xj maximizing rho^2(Y,Xj | selected Xi) is selected.
 #'
 #' @param X Matrix of predictors (n by dx)
 #' @param Y Vector of responses (n by dy)
@@ -202,26 +288,26 @@ KPCCME = function(Y, X = NULL, Z, ky, kx, kxz, eps, tol = 1e-3) {
 #' @param stop Stops at the first instance of negative Tn, if TRUE.
 #' @param numCores number of cores that are going to be used for parallelizing the process.
 #' @param k the kernel on Y: k(y,y')
-#' @param Knn the number of nearest neighbor
-#' @return a vector of the indices from 1,...,dx of the selected variables
+#' @param Knn the number of nearest neighbor; or "MST"
+#' @return A vector of the indices from 1,...,dx of the selected variables
 #' @examples
 #' n = 200
 #' p = 100
 #' set.seed(1)
 #' X = matrix(rnorm(n * p), ncol = p)
-#' Y = X[, 1] * X[, 2] + sin(X[, 1] * X[, 3]) + rnorm(n,sd = 0.5)
-#' Varselect_Knn(Y, X, kernlab::rbfdot(1), Knn=10, num_features=NULL, stop=TRUE, numCores=7)
+#' Y = X[, 1] * X[, 2] + sin(X[, 1] * X[, 3]) + rnorm(n)*0.5
+#' Varselect_Knn(Y, X, kernlab::rbfdot(1), Knn=10)
 ## It selects X1, X2, X3. FOCI selects X81
 # colnames(X) = paste0(rep("X", p), seq(1, p))
-# FOCI::foci(Y, X, numCores = 7)$selectedVar$index
+# FOCI::foci(Y, X, numCores = 1)$selectedVar$index
 #
 # Example:
 #surgical = olsrr::surgical
 #for (i in 1:9) {
 #  surgical[,i] = (surgical[,i] - mean(surgical[,i]))/sd(surgical[,i])
 #}
-#colnames(surgical)[Varselect_Knn(surgical[,9],surgical[,1:8],NULL,T,1,kernlab::rbfdot(1/(2*median(dist(surgical$y))^2)),1)]
-#### "enzyme_test" "pindex"      "liver_test"  "alc_heavy"
+#colnames(surgical)[Varselect_Knn(surgical[,9],surgical[,1:8],kernlab::rbfdot(1/(2*median(dist(surgical$y))^2)),1)]
+#### "enzyme_test" "pindex" "liver_test"  "alc_heavy"
 # code modified from Azadkia, M. and Chatterjee, S. (2019). A simple measure of conditional dependence.
 Varselect_Knn <- function(Y, X, k, Knn, num_features = NULL, stop = TRUE, numCores = 1){
   if(!is.matrix(X)) {
